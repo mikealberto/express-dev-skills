596464a081e1c37027095cc1539a88dcd5d15e40
mikealberto <alberto.michael1701@gmail.com>
2022-10-01 19:20:29 -0400

    add moreproperties in the skill model array

--- models/skill.js
+++ models/skill.js
@@ -1,7 +1,9 @@
 const skills = [
-  { skill: "Time Management" },
-  { skill: "Teamwork" },
-  { skill: "Organization" },
+  { id: 0456, skill: "Time Management", criticalSkill: true },
+  { id: 1243, skill: "Cooking", criticalSkill: false },
+  { id: 9832, skill: "Teamwork", criticalSkill: true },
+  { id: 7302, skill: "Skateboard Backflip", criticalSkill: false },
+  { id: 4710, skill: "Organization", criticalSkill: true },
 ];
 
 module.exports = { getAll };
