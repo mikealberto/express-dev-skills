5d233063ed0928fa2f4c113d47707b393761fe00
mikealberto <alberto.michael1701@gmail.com>
2022-10-01 18:22:32 -0400

    Create model/skill.js and add some developer skills

---
+++ models/skill.js
@@ -0,0 +1,11 @@
+const skills = [
+  { skill: "Time Management" },
+  { skill: "Teamwork" },
+  { skill: "Organization" },
+];
+
+module.exports = { getAll };
+
+function getAll() {
+  return skills;
+}
