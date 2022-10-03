f2c2a2d28e0eafa04bf74326f0d9f3b0f8bfa527
mikealberto <alberto.michael1701@gmail.com>
2022-10-03 14:50:31 -0400

    removal of critical skill property

--- models/skill.js
+++ models/skill.js
@@ -1,9 +1,9 @@
 const skills = [
-  { id: 7186, name: "Time Management", critical: true },
-  { id: 1243, name: "Cooking", critical: false },
-  { id: 9832, name: "Teamwork", critical: true },
-  { id: 7302, name: "Skateboard Backflip", critical: false },
-  { id: 4710, name: "Organization", critical: true },
+  { id: 7186, name: "Time Management" },
+  { id: 1243, name: "Cooking" },
+  { id: 9832, name: "Teamwork" },
+  { id: 7302, name: "Skateboard Backflip" },
+  { id: 4710, name: "Organization" },
 ];
 
 module.exports = { getAll, getOne };
