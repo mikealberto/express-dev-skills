a409930abee2ffd06f93a87142a2e2be3a41b802
mikealberto <alberto.michael1701@gmail.com>
2022-10-03 22:15:05 -0400

    initiate the delete route function so that it connects with the controller

--- controllers/skills.js
+++ controllers/skills.js
@@ -1,5 +1,5 @@
 const Skill = require("../models/skill");
-module.exports = { index, show, new: newSkill, create };
+module.exports = { index, show, new: newSkill, create, delete: deleteSkill };
 
 function index(req, res) {
   res.render("skills/index", {
@@ -18,5 +18,9 @@ function newSkill(req, res) {
 
 function create(req, res) {
   Skill.create(req.body);
+  res.redirect("/skills");
+}
+
+function deleteSkill(req, res) {
   res.redirect("/skills");
 }
